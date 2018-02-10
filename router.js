ce544c41d0f5138359278aa784970c08dacf9d96
Honchar Denys <crackeraki@gmail.com>
2018-02-10 02:44:54 +0200

    Update router.js

--- router.js
+++ router.js
@@ -1,6 +1,18 @@
 var CNAME = require(__dirname+'/schema.js');
-var api = '/api/NAME';
-module.exports = function(app, express, sd) {
-	var router = express.Router();
-	app.use(api, router);
+module.exports = function(sd) {
+	var router = sd._initRouter('/api/NAME');
+	/*sd['spNAMEensure'] = function(req, res, next){
+
+	}
+	sd['spNAMEqgp']
+	sd['spNAMEqg']
+	sd['spNAMEq'+update.name]
+	sd['spNAMEr']
+	sd['socketNAMEq']
+	sd['sp'+name+'ensure']
+	sd['sp'+name+'qgp']
+	sd['sp'+name+'qg']
+	sd['sp'+name+'q'+update.name]
+	sd['sp'+name+'r']
+	sd['socket' + cname + 'q']*/
 };
