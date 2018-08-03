8cb1ff955376557bbc9b44562f747fb7c1fd8ab7
Honchar Denys <crackeraki@gmail.com>
2018-08-03 16:27:49 +0300

    moved docs to readme.md

--- README.md
+++ README.md
@@ -26,3 +26,109 @@ Waw crud configuration continue here, permision for each update, create, remove 
 Everything what is in commends, code can be uncomment and modified. All those code are like that by default.
 
 ## waw crud
+##/api/NAME/create
+sd['ensure_create_NAME'] = function(req, res, next){
+	if(req.user) next();
+	else res.json(false);
+};
+
+##/api/NAME/get
+###Default Get Route Management
+sd['ensure_get_NAME'] = function(req, res, next){
+	next();
+};
+sd['populate_get_NAME'] = function(req, res){
+	return false;
+};
+sd['sort_get_NAME'] = function(req, res){
+	return false;
+};
+sd['skip_get_NAME'] = function(req, res){
+	return false;
+};
+sd['limit_get_NAME'] = function(req, res){
+	return false;
+};
+sd['select_get_NAME'] = function(req, res){
+	return false;
+};
+sd['query_get_NAME'] = function(req, res){
+	return {
+		moderators: req.user._id
+	}
+};
+###Custom Get Route Management with name 'all'
+sd['ensure_get_NAME_all'] = function(req, res, next){
+	next();
+};
+sd['populate_get_NAME_all'] = function(req, res){
+	return false;
+};
+sd['sort_get_NAME_all'] = function(req, res){
+	return false;
+};
+sd['skip_get_NAME_all'] = function(req, res){
+	return false;
+};
+sd['limit_get_NAME_all'] = function(req, res){
+	return false;
+};
+sd['select_get_NAME_all'] = function(req, res){
+	return false;
+};
+sd['query_get_NAME_all'] = function(req, res){
+	return {};
+};
+##/api/NAME/update
+sd['ensure_update_NAME'] = function(req, res, next){
+	next();
+};
+sd['query_update_NAME'] = function(req, res){
+	return {
+		_id: req.body._id,
+		moderators: req.user._id
+	};
+};
+##/api/NAME/update/all
+###Default Update All Route Management
+sd['ensure_update_all_NAME'] = function(req, res, next){
+	next();
+};
+sd['query_update_all_NAME'] = function(req, res){
+	return {
+		_id: req.body._id,
+		moderators: req.user._id
+	};
+};
+###Custom Update All Route Management with name 'all'
+sd['ensure_update_all_NAME_author'] = function(req, res, next){
+	next();
+};
+sd['query_update_all_NAME_author'] = function(req, res){
+	return {
+		_id: req.body._id,
+		moderators: req.user._id
+	};
+};
+
+##/api/NAME/unique/field
+sd['ensure_unique_field_NAME'] = function(req, res, next){
+	next();
+};
+sd['search_query_unique_field_NAME'] = function(req, res, update){
+	let query = {};
+	query[update.key] = req.body[update.key];
+	return query;
+};
+
+##/api/NAME/delete
+sd['ensure_delete_NAME'] = function(req, res, next){
+	if(req.user) next();
+	else res.json(false);
+};
+sd['delete_NAME'] = function(req, res){
+	return {
+		_id: req.body._id,
+		author: req.user._id
+	}
+}
--- router.js
+++ router.js
@@ -1,134 +1,16 @@
 var CNAME = require(__dirname+'/schema.js');
 module.exports = function(sd) {
 	var router = sd._initRouter('/api/NAME');
-	/*
-	*	/api/NAME/create
-	*	custom  query below code
-
-		// Ensure route
-		sd['ensure_create_NAME'] = function(req, res, next){
-			if(req.user) next();
-			else res.json(false);
+	sd['query_update_all_NAME_author'] = function(req, res){
+		return {
+			_id: req.body._id,
+			author: req.user._id
 		};
-	*/
-
-	/*
-	*	/api/NAME/get
-	*	custom  query below code
-
-		// Default Route Management
-		sd['ensure_get_NAME'] = function(req, res, next){
-			next();
+	};
+	sd['query_unique_field_NAME'] = function(req, res){
+		return {
+			_id: req.body._id,
+			author: req.user._id
 		};
-		sd['populate_get_NAME'] = function(req, res){
-			return false;
-		};
-		sd['sort_get_NAME'] = function(req, res){
-			return false;
-		};
-		sd['skip_get_NAME'] = function(req, res){
-			return false;
-		};
-		sd['limit_get_NAME'] = function(req, res){
-			return false;
-		};
-		sd['select_get_NAME'] = function(req, res){
-			return false;
-		};
-		sd['query_get_NAME'] = function(req, res){
-			return {
-				moderators: req.user._id
-			}
-		};
-
-
-		// Custom Route Management with name 'all'
-		sd['ensure_get_NAME_all'] = function(req, res, next){
-			next();
-		};
-		sd['populate_get_NAME_all'] = function(req, res){
-			return false;
-		};
-		sd['sort_get_NAME_all'] = function(req, res){
-			return false;
-		};
-		sd['skip_get_NAME_all'] = function(req, res){
-			return false;
-		};
-		sd['limit_get_NAME_all'] = function(req, res){
-			return false;
-		};
-		sd['select_get_NAME_all'] = function(req, res){
-			return false;
-		};
-		sd['query_get_NAME_all'] = function(req, res){
-			return {};
-		};
-	*/
-
-	/*
-	*	/api/NAME/update
-	*	custom  query below code
-
-		// Update
-		sd['ensure_update_NAME'] = function(req, res, next){
-			next();
-		};
-		sd['query_update_NAME'] = function(req, res){
-			return {
-				_id: req.body._id,
-				moderators: req.user._id
-			};
-		};
-	*/
-	
-	/*
-	*	/api/NAME/update/all
-	*	custom  query below code
-
-		// Update All
-		sd['ensure_update_all_NAME'] = function(req, res, next){
-			next();
-		};
-		sd['query_update_all_NAME'] = function(req, res){
-			return {
-				_id: req.body._id,
-				moderators: req.user._id
-			};
-		};
-
-		// Update All AUthor
-		sd['ensure_update_all_NAME_author'] = function(req, res, next){
-			next();
-		};
-	*/
-		sd['query_update_all_NAME_author'] = function(req, res){
-			return {
-				_id: req.body._id,
-				author: req.user._id
-			};
-		};
-
-	/*
-	*	/api/NAME/delete
-	*	custom  query below code
-
-		// Ensure route
-		sd['ensure_delete_NAME'] = function(req, res, next){
-			if(req.user) next();
-			else res.json(false);
-		};
-
-		// Query for delete
-		sd['delete_NAME'] = function(req, res){
-			return {
-				_id: req.body._id,
-				author: req.user._id
-			}
-		}
-	*/
-
-	/*
-	*	End of router
-	*/
+	};
 };
