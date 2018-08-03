b2c6f4d01e0db74d44dd447667e6e015f8c66a58
Honchar Denys <crackeraki@gmail.com>
2018-08-03 16:05:25 +0300

    Update schema.js

--- schema.js
+++ schema.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema({
 	name: String,
 	description: String,
 	author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-	moderators: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+	moderators: [{type: mongoose.Schema.Types.ObjectId, sparse: true, ref: 'User'}],
+	url: {type: String, unique: true, sparse: true, trim: true}
 });
 
 Schema.methods.create = function(obj, user, sd) {
